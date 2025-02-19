feffe57aed1690c4d2322148874ac15d135ca683
Lafatra13 <lafatraharimanantsoa@gmail.com>
2025-02-19 09:50:14 +0300

    SommeDesNPremiersEntiers.c

--- SommeDesNPremiersEntiers.c
+++ SommeDesNPremiersEntiers.c
@@ -8,17 +8,17 @@ int main ()
     printf("Entrer un nombre:") ;
     scanf("%d", &N) ;
     
-  for(i=1;i<N;i++) 
+  for(i=1;i<=N;i++) 
   {
     S=S+i;
     printf("%d",i) ;
-    if(i<(N-1)) 
+    if(i<N) 
     {
       printf("+") ;
     }  
   }
-  S=S+N;
-  printf("+%d=%f",N, S) ;
+  
+  printf("=%f",S) ;
   
  return (0);
 }
